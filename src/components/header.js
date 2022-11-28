5429f830afa6d71f5f7256ddbe6ba3a7ce1ce05f
guixou <guillaumejuju@live.fr>
2022-11-28 21:52:28 +0100

    Header
    Finalisation du header et du Active header

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Linkedin from '../assets/svg/linkedin.svg';
 import Cv from '../assets/svg/cv.svg';
 import Github from '../assets/svg/github.svg';
-import HoverNav from '../assets/svg/hoverNav.svg'
+import ActiveNav from '../assets/svg/activeNav.svg'
+import DisableNav from '../assets/svg/disableNav.svg'
 import '../styles/header.css'
 
 function Header() {
@@ -10,33 +11,47 @@ function Header() {
     
 
     return isOpen ? (
-        <div className='header'>
-            <h2>Guillaume picard</h2>
-                <button 
-                    className='HoverNav'
-                    onClick={() => setIsOpen(false)} 
-                >
-                    <img src={HoverNav} alt=""/>
-                </button>
-                
-            <nav>
-                <a href="">Accueil</a>
-                <a href="">Parcour</a>
-                <a href="">Portfolio</a>
-            </nav>
-            <div className='reseau'>
-                <img src={Linkedin} alt="acéder à mon Linkedin" />
-                <img src={Cv} alt="acéder à Mon Cv" />
-                <img src={Github} alt="acéder à mon Github" />
+        <React.Fragment>
+            <div className='header'>
+                <h2>Guillaume picard</h2>
+                                    
+                <nav>
+                    <a href="">Accueil</a>
+                    <a href="">Parcour</a>
+                    <a href="">Portfolio</a>
+                    <a href="">Contact</a>
+                </nav>
+
+                <div className='reseau'>
+                    <a href="https://www.linkedin.com/in/guillaume-picard-9513aa1b5/">
+                        <img src={Linkedin} alt="acéder à mon Linkedin" />
+                    </a>
+                    <a href="">
+                        <img src={Cv} alt="acéder à Mon Cv" />
+                    </a>
+                    <a href="https://github.com/guixou">
+                        <img src={Github} alt="acéder à mon Github" />
+                    </a>
+                </div>
             </div>
-        </div>
+
+            <button 
+                className='activeNav'
+                onClick={() => setIsOpen(false)} 
+            >
+                <img src={DisableNav} alt=""/>
+            </button>
+        </React.Fragment>
     ) : (
-        <button 
-            className='HoverNav'
-            onClick={() => setIsOpen(true)}   
-        >
-            <p>ouvert</p>
-        </button>
+        <div className='header headerActive'>
+            <h2>Guillaume picard</h2>
+            <button
+                className='disableNav'
+                onClick={() => setIsOpen(true)}
+            >
+                <img src={ActiveNav} alt="" />
+            </button>
+        </div>
     );
 
 }
