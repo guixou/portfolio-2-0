95873341255d069f821f7cc584eabe2c5886cd59
guixou <guillaumejuju@live.fr>
2022-12-01 13:34:42 +0100

    Update capiCrea.js

--- src/components/creation/capiCrea.js
+++ src/components/creation/capiCrea.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Site from '../../assets/site/CapiCrea/index.html';
-import html from '../../assets/site/CapiCrea/index.html';
 
 function capiCrea() {
     return (
@@ -8,7 +6,7 @@ function capiCrea() {
             <h4>Capi Creative</h4>
             <p>Pub pour une ablication de trading/cryptoactif</p>
             <p>Site/PUB fictive, défi intégration en moins d'une jourée réussi</p>
-            <a href={Site} target="_blank">Visiter le site</a>
+            <a href="" target="_blank">Visiter le site</a>
         </div>
     );
 }
