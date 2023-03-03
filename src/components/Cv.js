0e4f1dee25aa5ace0834942ca6173e8142c47638
Guillaume <guillaumejuju@live.fr>
2023-03-03 21:05:28 +0100

    Lien CV
    Correctif sur le téléchargement du bon CV

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -5,7 +5,7 @@ import Cvpdf from '../assets/pdf/cv.pdf'
 export default function Cv() {
     return (
         <div className='cv'>
-            <a href="CV.pdf" download={Cvpdf}>Télécharger <img src={Download} alt="Télécharger mon CV" /></a>
+            <a href={Cvpdf} download="Cv Guillaume Picard">Télécharger <img src={Download} alt="Télécharger mon CV" /></a>
             <a href={Cvpdf} target = "noopener">Voir en ligne</a>
         </div>
     );
